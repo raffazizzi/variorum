1d688984ac14be3296bf8d9c537471ebc451e19d
raffazizzi <raffaeleviglianti@gmail.com>
2017-03-17 13:44:34 -0400

    Using source label in the sidebar

--- src/js/templates/TEIfiles-tpl.js
+++ src/js/templates/TEIfiles-tpl.js
@@ -2,7 +2,7 @@ import * as Handlebars from 'handlebars';
 
 let TEIfiles_tpl = `
 {{#each . }}
-<li {{#if this.selected}}class="active"{{/if}}><a href="#source/{{source}}">{{source}}</a></li>
+<li {{#if this.selected}}class="active"{{/if}}><a href="#source/{{source}}">{{label}}</a></li>
 {{/each}}`
 
 export default Handlebars.compile(TEIfiles_tpl);
